e644a851a980e00fab443e969ecd2fda7b724b76
ajk024 <andrew.kozbial@gmail.com>
2026-07-30 00:01:29 -0400

    updates

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from enum import Enum
+from htmlnode import *
 
 
 class TextType(Enum):
@@ -26,3 +27,9 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type not in TextType:
+        raise Exception("text_node text_type not in TextType")
+
+    
